88e4a7038d7226f7a278cd6e24ca24c891d33311
Fiona <fiona.moeller05@gmail.com>
2026-09-11 19:41:25 +0200

    Initial commit

---
+++ backend/src/test/java/com/scrumpoker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumpoker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
