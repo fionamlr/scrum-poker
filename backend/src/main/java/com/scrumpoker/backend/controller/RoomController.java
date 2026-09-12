713a06a6241896450e987ba6266a520d0e6daf47
Fiona <fiona.moeller05@gmail.com>
2026-09-12 09:21:40 +0200

    scrum-poker-be: bypass the CORS rule

--- backend/src/main/java/com/scrumpoker/backend/controller/RoomController.java
+++ backend/src/main/java/com/scrumpoker/backend/controller/RoomController.java
@@ -2,6 +2,7 @@ package com.scrumpoker.backend.controller;
 
 import com.scrumpoker.backend.dto.RoomDTO;
 import com.scrumpoker.backend.service.RoomService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/rooms")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RoomController {
     private final RoomService roomService;
 
